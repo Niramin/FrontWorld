e71c6e5321d1aa17100e1350c1975593aeb81619
Shashwat Ratna <shashwatratna@gmail.com>
2023-07-20 21:05:26 +0530

    Add project files.

---
+++ FrontWorld/ExchangeItem/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontWorld.ExchangeItem
+{
+    public class Item
+    {
+        public string name { get; set; }
+
+        public string country { get; set; }
+
+        public int price { get; set; }
+
+        public int quantity { get; set; }
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+    }
+}
